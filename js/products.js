6807b36370066dc7bc2a4a4fc6e885233be7af24
Diego Quiroga <quirogadiego2603@gmail.com>
2023-09-20 17:21:15 -0500

    Se añadio nueva forma de ver los items en el carrito

--- js/products.js
+++ js/products.js
@@ -1,51 +1,64 @@
 export const products = {
     product1: {
       descripcion: "Papel natural tamaño carta, 100 % de fibra de caña de azúcar.",
-      precio: "$21.900",
+      precio: "21900",
+      img: "img/img_p01.jpeg"
+
     },
     product2: {
       descripcion: "• Block oficio Jean Book en papel bond de 56 gramos • Carátula en cartulina esmaltada • 70 hojas blancas • Tamaño: 20.8 cm x 33.0 cm",
-      precio: "$6.500",
+      precio: "6500",
+      img: "img/img_p02.jpeg"
     },
     product3: {
       descripcion: "• 450 hojas • Colores tropicales • 5 colores de 90 hojas cada uno • Medidas: 7.6 x 7.6 cm",
-      precio: "$28.200",
+      precio: "28200",
+      img: "img/img_p03.jpeg"
     },
     product4: {
       descripcion: "• 25 x 5 banderitas rectangulares • Colores neón",
-      precio: "$7.900",
+      precio: "7900",
+      img: "img/img_p04.jpeg"
     },
     product5: {
       descripcion: "Acetato para laminar • Láminas calibre 5 de 125 micras • Elaboradas en material PVC de alta resistencia • Tamaño oficio",
-      precio: "$17.900",
+      precio: "17900",
+      img: "img/img_p05.jpeg"
     },
     product6: {
       descripcion: "• Notas adhesivas por 5 packs de colores • Cada pack cuenta con 100 hojas • Tamaño 76 mm x 127mm",
-      precio: "$26.500",
+      precio: "26500",
+      img: "img/img_p06.jpeg"
     },
     product7: {
       descripcion: "• Adhesivo resistente • Color: blanco • Tamaño: 8.8 cm x 2.6 cm • Paquete por 110 unidades",
-      precio: "$4.300",
+      precio: "4300",
+      img: "img/img_p07.jpeg"
     },
     product8: {
       descripcion: "Papel bond de alta blancura. Fabricado con tecnología alcalina de 75 gramos.",
-      precio: "$23.900",
+      precio: "23900",
+      img: "img/img_p08.jpeg"
     },
     product9: {
       descripcion: "• 4 unidades • Color dorado • Tamaño aprox.: 32 mm c/u",
-      precio: "$7.900",
+      precio: "7900",
+      img: "img/img_p09.jpeg"
     },
     product10: {
       descripcion: "Archivador tipo fuelle colgante Beautone • Tamaño: A4 • 3 bolsillos • Color amarillo",
-      precio: "$8.900",
+      precio: "8900",
+      img: "img/img_p10.jpeg"
     },
     product11: {
       descripcion: "Lomo ancho en papel de encuadernación para una mayor durabilidad, evita el desgaste lateral del producto. Elaborado en cartón rígido y reforzado con dos biseles en el borde inferior.",
-      precio: "$8.300",
+      precio: "8300",
+      img: "img/img_p11.jpeg"
     },
     product12: {
       descripcion: "• Para clasificar hojas sueltas y documentos en forma eficiente; facilidad para abrir. • En colores amarillo, azul, verde, rosado, humo y transparente. • Paquete x 6 unidades. • Tamaño: carta. • Material: plástico.",
-      precio: "$11.600",
+      precio: "11600",
+      img: "img/img_p12.jpeg"
     },
   };
 
--- js/script.js
+++ js/script.js
@@ -8,13 +8,18 @@ console.log(products);
 let itemsCart = JSON.parse(localStorage.getItem("itemsCart"))
                 ??
                 {}
-
+console.log(itemsCart)
 addEventListener("DOMContentLoaded", (event) => {
 
 });
 
 
-const carroBtn = document.getElementById('carro');  
+const carroBtn = document.getElementById('carro'); 
+const btnProducts = document.querySelectorAll(".product");
+
+ btnProducts.forEach((btn) => {
+    btn.addEventListener("click", addProduct)
+ })
 
 
 // Mostrar el modal del carrito cuando se hace clic en el botón del carrito
@@ -31,12 +36,96 @@ carroBtn.addEventListener('click', function () {
 
 });
 
-
 function addProduct() {
-    const 
+    /*console.log(this.id) */
+    const nameProduct = this.id;
+    const keysProductsCart = Object.keys(itemsCart);
+    if (keysProductsCart.includes(nameProduct)) {
+        /* Se define la propeidad cantidad */
+        let cantidad = itemsCart[nameProduct].cantidad ?? 0;
+        let valor = parseFloat(products[nameProduct].precio)
+        
+
+        /* Se establece nueva cantidad */
+        itemsCart[nameProduct].cantidad = cantidad + 1;
+        /* Se establece neuvo valor */
+        valor = ((cantidad + 1) * valor);
+
+        /* Se asigna nuevo valor */
+        itemsCart[nameProduct].valor = valor
+
+        /* console.log(formatNumber(valor)) */
+        localStorage.setItem("itemsCart", JSON.stringify(itemsCart));
+        console.log(itemsCart);
+
+    }
+    else {
+        let valor = parseFloat(products[nameProduct].precio)
+        let cantidad = 1;
+        /* S */
+        itemsCart[nameProduct] = {};
+        itemsCart[nameProduct]["cantidad"] = cantidad;
+        itemsCart[nameProduct]["valor"] = valor;
+        localStorage.setItem("itemsCart", JSON.stringify(itemsCart));
+        console.log(itemsCart);
+    }
+}
+
+
+
+function showCartItems(){
+    const cartContent = document.querySelector(".cart-content");
+    const cartItems = document.querySelector(".cart-items")
+    let html = "";
+    const closeCartBtn = document.getElementById('cardButton-close');
+    for (let productItem in itemsCart) {
+        html += `
+        <td></td>
+        <img src="${products[productItem].img}" alt="">              
+        <td>
+            <img class="product__image">
+        </td>
+        <!-- aquí titulo, descripción y slider cantidad -->
+        <td>
+            <!-- titulo y descripción-->
+            <tr>
+                <h3>${products[productItem].descripcion}</h3>
+                <p>Cantidad: ${itemsCart[productItem].valor}</p>
+                <p>Precio: ${products[productItem].precio}</p>
+            </tr>
+        </td>`
+        /* html += `<img src="${products[productItem].img}" alt="">
+        <div>
+            <h3>${products[productItem].descripcion}</h3>
+            <p>Precio: ${products[productItem].precio}</p>
+            <p>Cantidad: ${itemsCart[productItem].valor}</p>
+        </div>`; */
+    }
+    cartItems.innerHTML = html
+
+}
+ 
+function showTotalValue() {
 
 }
 
 /* module.exports = datos; //Especificar valroes del objeto */
 
 /*  localStorage.setItem("categorias", JSON.stringify(categorias)); */
+
+/* localStorage.removeItem("itemsCart"); */
+
+function formatNumber(number) {
+    /* Funcion que se encarga de poner los puntos en el numero */
+
+    /* const formattedNumber = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(Number(number));
+   */
+    const formattedNumber =  new Intl.NumberFormat('de-DE').format(number)
+    return formattedNumber;
+}
+
+localStorage.removeItem("itemsCart");
+
