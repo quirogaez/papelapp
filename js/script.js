c583ec5458c8a650cf2be935eb096d58c7b489c2
Diego Quiroga <quirogadiego2603@gmail.com>
2023-09-20 22:51:36 -0500

    Ultimos comentarios al archivo script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 /* Se importa el objeto que actua como base de datos de los productos */
 import { products } from "./products.js";
 
-console.log(products);
+
 
 /* Se inicializa itemsCart con el valor guardado en el localstorage, sino, se inicializa como un objeto vacio */
 let itemsCart = JSON.parse(localStorage.getItem("itemsCart"))
                 ??
                 {}
-console.log(itemsCart)
+
 
 addEventListener("DOMContentLoaded", (event) => {
     /* Si existe productos en el objeto del local storage se pinta el numero de productos en el carrito */
@@ -22,6 +22,8 @@ addEventListener("DOMContentLoaded", (event) => {
 const carroBtn = document.getElementById('carro'); 
 /* Se selecciona todos los botones de lso productos */
 const btnProducts = document.querySelectorAll(".product");
+/* Se selecciona el boton para borrar carrito */
+const btnEmpty = document.getElementById("cardButton-empty");
 /* Se le agrega un evento a cada producto */
 btnProducts.forEach((btn) => {
     btn.addEventListener("click", addProduct)
@@ -30,11 +32,14 @@ btnProducts.forEach((btn) => {
 
 // Mostrar el modal del carrito cuando se hace clic en el botón del carrito
 carroBtn.addEventListener('click', function () {
+    /* Se selecciona el contenedor que se encuentra oculto */
     const cartModal = document.getElementById('cart-modal');
     const closeCartBtn = document.getElementById('cardButton-close');
+    /* Se quita el oculto de contenedor */
     cartModal.style.display = 'block';
-    /* Mostrar items en el carrito */
+    /* Mostrar-pintar items en el carrito */
     showCartItems();
+    /* Mostrar-Pintar valor total */
     showTotalValue();
     // Ocultar el modal del carrito cuando se hace clic en el botón de cierre
     closeCartBtn.addEventListener('click', function () {
@@ -42,6 +47,22 @@ carroBtn.addEventListener('click', function () {
     });
 
 });
+
+/* Evento que se dispara cuando se da click a boton vaciar carrito */
+btnEmpty.addEventListener('click', function () {
+    /* Se selecciona contenedor con los carts */
+    const cartItems = document.querySelector(".cart-items");
+    /* Se vacia contenedor */
+    cartItems.innerHTML = " ";
+    /* Se reinicia el objeto con los productos del carrito */
+    itemsCart = {}
+    /* Se persiste en el local storage */
+    localStorage.setItem("itemsCart", JSON.stringify(itemsCart));
+    /* Se recarga el valor de items en el carrito */
+    loadNumberCart();
+    /* Se recarga el valor total del carrito */
+    showTotalValue();
+})
 
 /* Funciónes para el carrito de compras */
 function addProduct() {
@@ -66,12 +87,12 @@ function addProduct() {
         valor = ((cantidad + 1) * valor);
 
         /* Se asigna nuevo valor según su cantidad*/
-        itemsCart[nameProduct].valor = valor
+        itemsCart[nameProduct].valor = valor;
 
         /* console.log(formatNumber(valor)) */
         /* Se persiste en el local storage */
         localStorage.setItem("itemsCart", JSON.stringify(itemsCart));
-        console.log(itemsCart);
+        /* Actualizar numero de items en el carrito */
         plusProductsCart(1);
 
     } else {
@@ -80,13 +101,13 @@ function addProduct() {
         let valor = parseFloat(products[nameProduct].valor)
         /* Se añade uno al carrito */
         let cantidad = 1;
-        /* S */
+        /* Se crea nuevo objeto dentro de itemsCart*/
         itemsCart[nameProduct] = {};
         itemsCart[nameProduct]["cantidad"] = cantidad;
-        itemsCart[nameProduct]["valor"] = valor
+        itemsCart[nameProduct]["valor"] = valor;
         /* Se persiste en el local storage */
         localStorage.setItem("itemsCart", JSON.stringify(itemsCart));
-        console.log(itemsCart);
+         /* Actualizar numero de items en el carrito */
         plusProductsCart(1);
     }
 }
@@ -94,12 +115,14 @@ function addProduct() {
 
 
 function showCartItems(){
-    /* Funcion que pinta todos lso productos seleccionados en el carrito */
-    const cartContent = document.querySelector(".cart-content");
-    const cartItems = document.querySelector(".cart-items")
+    /* Funcion que pinta todos los productos seleccionados en el carrito */
+    /* Se selecciona el contenedor de los items del carrito de compras  */
+    const cartItems = document.querySelector(".cart-items");
+    /* Inicialziar cotenido html donde se podnrán lso carts */
     let html = "";
-    const closeCartBtn = document.getElementById('cardButton-close');
+    /* For para recorrer las propeidades del objeto itemsCart */
     for (let productItem in itemsCart) {
+        /* Se acumulan todos carts */
         html += `
         <div class="shopcart-item">
             <img class="shopproduct__image" src="${products[productItem].img}" alt="Producto">
@@ -118,24 +141,31 @@ function showCartItems(){
             <p>Cantidad: ${itemsCart[productItem].valor}</p>
         </div>`; */
     }
-    cartItems.innerHTML = html
+    /* Se añaden los carts al carrito */
+    cartItems.innerHTML = html;
     return
 }
  
 function showTotalValue() {
+    /* Esta funcion se encarga de pintar el valor total en el carrito */
     const totalValueDom = document.querySelector(".total-value");
     let totalValue = 0;
+    /* Se inicialzia un for que sumará todos lso valores */
     for (let product in itemsCart) {
+         /* Se acumula el valor de todos los productos (total) */
         totalValue +=  itemsCart[product].valor;
     }
+    /* Se agrega al valor total con formato español */
     totalValueDom.textContent = formatNumber(totalValue);
     return
 }
 
 function loadNumberCart() {
+    /* Funcion encargada de cargar el numero de productos dentro del carrito */
     const numItems = document.querySelector(".num-items");
     let quantityTotal = 0;
     for (let product in itemsCart) {
+        /* Se acumula el numero de items por cada producto */
         quantityTotal +=  itemsCart[product].cantidad;
     }
     numItems.textContent = quantityTotal;
@@ -144,30 +174,25 @@ function loadNumberCart() {
 
 
 function plusProductsCart(value) {
+    /* Funcion encargada de sumar el numero de productos dentro del carrito con lso valores actuales */
     /* Numero de productos dentro del shopping cart */
     const numItems = document.querySelector(".num-items");
     let numProductsCart = parseInt(numItems.textContent);
-    /* Hacer operacion */
+    /* Hacer operacion que sumará el numero de productos actual con value */
     numProductsCart = numProductsCart + value;
-    console.log(numProductsCart)
-    /* Asignar neuvo valro al centenedor */
+    /* Asignar nuevo valro al centenedor */
     numItems.textContent = numProductsCart;
     return
 }
+
+
 /* Aquí terminan las funciónes para el carrito de compras */
 
 
 
-
-/* module.exports = datos; //Especificar valroes del objeto */
-
-/*  localStorage.setItem("categorias", JSON.stringify(categorias)); */
-
-/* localStorage.removeItem("itemsCart"); */
-
 function formatNumber(number) {
     /* Funcion que se encarga de poner los puntos en el numero */
-    const formattedNumber =  new Intl.NumberFormat('de-DE').format(number)
+    const formattedNumber =  new Intl.NumberFormat('de-DE').format(number);
     return formattedNumber;
 }
 
